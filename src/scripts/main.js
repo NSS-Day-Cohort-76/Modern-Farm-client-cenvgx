245ed10942cd9e91ace60580f045936028f6418e
Brandon Hickman <brandon_7_77@hotmail.com>
2025-03-20 14:04:38 -0500

    Added fields module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,27 @@
 console.log("Welcome to the main module")
-import { createPlan } from "./plan.js"
-
+//import { createPlan } from "./plan.js"
+import { createAsparagus } from "./seeds/asparagus.js";
+import { createCorn } from "./seeds/corn.js";
+import { createPotato } from "./seeds/potato.js";
+import { createSoybean } from "./seeds/soybean.js";
+import { createSunflower } from "./seeds/sunflower.js";
+import { createWheat } from "./seeds/wheat.js";
 // Invoke the createPlan function and store the return value in yearlyPlan
-const yearlyPlan = createPlan();
+// const yearlyPlan = createPlan();
 
-console.log(yearlyPlan);
+// console.log(yearlyPlan);
+
+const asparagusSeed = createAsparagus()
+const potatoSeed = createPotato()
+const soybeanSeed = createSoybean()
+const sunflowerSeed = createSunflower()
+const wheatSeed = createWheat()
+const cornSeed = createCorn()
+
+
+console.log(asparagusSeed);
+console.log(potatoSeed);
+console.log(soybeanSeed);
+console.log(sunflowerSeed);
+console.log(wheatSeed);
+console.log(cornSeed);
