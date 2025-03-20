2de2324d3b8c05b0fa0ea5d69a2a0af3ddad1c4e
Brandon Hickman <brandon_7_77@hotmail.com>
2025-03-20 13:25:41 -0500

    Added asparagus module

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,7 @@
 console.log("Welcome to the main module")
+import { createPlan } from "./plan.js"
 
+// Invoke the createPlan function and store the return value in yearlyPlan
+const yearlyPlan = createPlan();
+
+console.log(yearlyPlan);
